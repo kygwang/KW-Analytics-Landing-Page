a2414287de66d2712a0e4c529c6fae16588ea57e
kygwang <kyle.g.wang@gmail.com>
2022-11-21 13:51:14 -0500

    Updating index and redirects

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ export default function Home() {
       <main>
         <Header title="Welcome to KW Analytics!" />
         <p className="description">
-          Under maintenance - coming soon!
-          You can learn more about <a href="https://www.linkedin.com/in/kylegwang">Kyle Wang</a>
+          Under maintenance - coming soon! <br />
+          For now, learn more about <a href="https://www.linkedin.com/in/kylegwang">Kyle Wang on Linkedin</a>
         </p>
       </main>
 
