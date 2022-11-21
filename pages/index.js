7fd584defed5bd8d48f83f0aafa73d56faa582bd
Kyle Wang <kygwang@users.noreply.github.com>
2022-11-21 07:57:17 -0500

    Update index.js
    Removed footer + updated description

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 import Head from 'next/head'
 import Header from '@components/Header'
-import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Welcome - KW Analytics Inc.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to KW Analytics!" />
         <p className="description">
+          Kyle Wang's website <br>
           Under maintenance - coming soon!
         </p>
       </main>
